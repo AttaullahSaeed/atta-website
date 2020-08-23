1df214dc7ca6be6cddbd108f6c335b0506d007ed
Atta Ullah Saeed <attaullahsaeed77@gmail.com>
2020-08-23 15:51:17 +0500

    Updates

---
+++ precache-manifest.07f0eb3389408666584056ee3f6e0692.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22efe4290d122474bbeeb15fbfc15034",
+    "url": "/atta-website/index.html"
+  },
+  {
+    "revision": "f002bce295bd19f7d575",
+    "url": "/atta-website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "b1f4b66e7b2e5524e7b9",
+    "url": "/atta-website/static/css/main.fc149a6c.chunk.css"
+  },
+  {
+    "revision": "f002bce295bd19f7d575",
+    "url": "/atta-website/static/js/2.47978aa4.chunk.js"
+  },
+  {
+    "revision": "dd8c0453bd67f3c5389d7862555b87e5",
+    "url": "/atta-website/static/js/2.47978aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f4b66e7b2e5524e7b9",
+    "url": "/atta-website/static/js/main.07590a66.chunk.js"
+  },
+  {
+    "revision": "19b06a4a802c18c24a0e",
+    "url": "/atta-website/static/js/runtime-main.ec25d724.js"
+  },
+  {
+    "revision": "b4daaa569686dc4c695696015f6b4181",
+    "url": "/atta-website/static/media/ars.b4daaa56.jpg"
+  },
+  {
+    "revision": "d2a3c475765eb6d87493b2db950701a1",
+    "url": "/atta-website/static/media/atta.d2a3c475.jpg"
+  },
+  {
+    "revision": "56e3646eaf780b382b75f435fd1a1692",
+    "url": "/atta-website/static/media/atta1.56e3646e.jpg"
+  },
+  {
+    "revision": "09f553d9a896826818745df176a6385f",
+    "url": "/atta-website/static/media/lhr.09f553d9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/atta-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "15ac2b3aaa74314814e5b75f87313bba",
+    "url": "/atta-website/static/media/meo.15ac2b3a.jpg"
+  },
+  {
+    "revision": "970468e8b9bd660e5513677399c5965d",
+    "url": "/atta-website/static/media/meo1.970468e8.jpg"
+  },
+  {
+    "revision": "089217e528e68335502a8393ea199531",
+    "url": "/atta-website/static/media/meo3.089217e5.jpg"
+  },
+  {
+    "revision": "c7f310b011713c76d61749c4e7136bd4",
+    "url": "/atta-website/static/media/meo4.c7f310b0.jpg"
+  },
+  {
+    "revision": "4b837e5e1fdffe0208b557eddcc06223",
+    "url": "/atta-website/static/media/meo5.4b837e5e.jpg"
+  },
+  {
+    "revision": "e225b1469550e3a0f75885574d390b11",
+    "url": "/atta-website/static/media/meo6.e225b146.jpg"
+  },
+  {
+    "revision": "d85b4cde4ebb9498ebe696d0f5eff993",
+    "url": "/atta-website/static/media/mol.d85b4cde.jpg"
+  },
+  {
+    "revision": "92fd313bd3b3e17ffc2a38f55e67c40e",
+    "url": "/atta-website/static/media/ol.92fd313b.jpg"
+  },
+  {
+    "revision": "2be2f1097640466ad14803835c36eb8d",
+    "url": "/atta-website/static/media/t2.2be2f109.jpg"
+  },
+  {
+    "revision": "2f21a85b8774ab455da794e6cc96c815",
+    "url": "/atta-website/static/media/tariq.2f21a85b.jpg"
+  },
+  {
+    "revision": "84e7ff4705f6a9d67ce486fe7dbc376e",
+    "url": "/atta-website/static/media/umar.84e7ff47.jpg"
+  }
+]);
